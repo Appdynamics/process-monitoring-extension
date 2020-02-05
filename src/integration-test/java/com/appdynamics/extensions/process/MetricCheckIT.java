f7e30400c8c90033d938f63c11dbc9bf7f0164b1
bhuvnesh17 <raghur171@gmail.com>
2020-02-04 16:35:44 -0800

    updates

---
+++ src/integration-test/java/com/appdynamics/extensions/process/MetricCheckIT.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright 2020 AppDynamics LLC and its affiliates
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.appdynamics.extensions.process;
+
+import com.appdynamics.extensions.controller.apiservices.CustomDashboardAPIService;
+import com.appdynamics.extensions.controller.apiservices.MetricAPIService;
+import com.appdynamics.extensions.util.JsonUtils;
+import org.codehaus.jackson.JsonNode;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+public class MetricCheckIT {
+    private MetricAPIService metricAPIService;
+    private CustomDashboardAPIService customDashboardAPIService;
+
+    @Before
+    public void setup() {
+        metricAPIService = IntegrationTestUtils.initializeMetricAPIService();
+        customDashboardAPIService = IntegrationTestUtils.initializeCustomDashboardAPIService();
+    }
+
+    @Test
+    public void whenInstanceIsUpThenHeartBeatIs1ForServer() {
+        JsonNode jsonNode = null;
+        if (metricAPIService != null) {
+            jsonNode = metricAPIService.getMetricData("",
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CCassandra%7CLocal%20Cassandra%20Server%201%7CHeart%20Beat&time-range-type=BEFORE_NOW&duration-in-mins=60&output=JSON");
+        }
+        Assert.assertNotNull("Cannot connect to controller API", jsonNode);
+        if (jsonNode != null) {
+            JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "*", "metricValues", "*", "value");
+            int heartBeat = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertEquals("heartbeat is 0", heartBeat, 1);
+        }
+    }
+
+    @Test
+    public void checkTotalNumberOfMetricsReportedIsGreaterThan1() {
+        JsonNode jsonNode = null;
+        if (metricAPIService != null) {
+            jsonNode = metricAPIService.getMetricData("",
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CCassandra%7CMetrics%20Uploaded&time-range-type=BEFORE_NOW&duration-in-mins=15&output=JSON");
+        }
+        Assert.assertNotNull("Cannot connect to controller API", jsonNode);
+        if (jsonNode != null) {
+            JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "*", "metricValues", "*", "value");
+            int totalNumberOfMetricsReported = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertTrue(totalNumberOfMetricsReported > 1);
+        }
+    }
+
+    @Test
+    public void whenAliasIsAppliedThenCheckMetricName() {
+        JsonNode jsonNode = null;
+        if (metricAPIService != null) {
+            jsonNode = metricAPIService.getMetricData("",
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CCassandra%7CLocal%20Cassandra%20Server%201%7CCache%7CChunkCache%7CSize%7CCache%20Size&time-range-type=BEFORE_NOW&duration-in-mins=60&output=JSON");
+        }
+        Assert.assertNotNull("Cannot connect to controller API", jsonNode);
+        if (jsonNode != null) {
+            JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "metricName");
+            String metricName = (valueNode == null) ? "" : valueNode.get(0).toString();
+            int metricValue = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertEquals("Metric alias is invalid", "\"Custom Metrics|Cassandra|Local Cassandra Server 1|Cache|ChunkCache|Size|Cache Size\"", metricName);
+            Assert.assertNotNull("Metric Value is  null in last 15min, maybe a stale metric ", metricValue);
+        }
+    }
+
+    @Test
+    public void checkDashboardsUploaded() {
+        boolean dashboardPresent = false;
+        if (customDashboardAPIService != null) {
+            JsonNode allDashboardsNode = customDashboardAPIService.getAllDashboards();
+            dashboardPresent = IntegrationTestUtils.isDashboardPresent("Cassandra SIM Dashboard", allDashboardsNode);
+            Assert.assertTrue(dashboardPresent);
+        } else {
+            Assert.assertTrue(false);
+        }
+    }
+
+    @Test
+    public void checkMetricCharReplaced() {
+        JsonNode jsonNode = null;
+        if (metricAPIService != null) {
+            jsonNode = metricAPIService.getMetricData("",
+                    "Server%20&%20Infrastructure%20Monitoring/metric-data?metric-path=Application%20Infrastructure%20Performance%7CRoot%7CCustom%20Metrics%7CCassandra%7CLocal%20Cassandra%20Server%201%7CCommit%20Log%7CCompletedTasks%7CNumber%20of%20Completed%20Tasks&time-range-type=BEFORE_NOW&duration-in-mins=60&output=JSON");
+        }
+        Assert.assertNotNull("Cannot connect to controller API", jsonNode);
+        if (jsonNode != null) {
+            JsonNode valueNode = JsonUtils.getNestedObject(jsonNode, "metricName");
+            String metricName = (valueNode == null) ? "" : valueNode.get(0).toString();
+            int metricValue = (valueNode == null) ? 0 : valueNode.get(0).asInt();
+            Assert.assertEquals("Metric char replacement is not done", "\"Custom Metrics|Cassandra|Local Cassandra Server 1|Commit Log|CompletedTasks|Number of Completed Tasks\"", metricName);
+            Assert.assertNotNull("Metric Value is  null in last 15min, maybe a stale metric ", metricValue);
+        }
+    }
+
+}
